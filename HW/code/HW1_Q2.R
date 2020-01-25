a021394007d7dfe619fbcac6579204dd1dea7ee7
Yongseok <ysugk@snu.ac.kr>
2020-01-24 23:19:02 -0500

    Modify codes

--- HW/code/HW1_Q2.R
+++ HW/code/HW1_Q2.R
@@ -1,20 +1,42 @@
-library(tidyverse)
+#' ---
+#' title: ""
+#' author: ""
+#' date: ""
+#' output: pdf_document
+#' ---
+#' 
 
+
+#' Generate random sample
 set.seed(2020)
-
 X <- rnorm(n = 100, mean = 1, sd = 2)
 
-log_likelihood_function <- function(mu, sigma, x) {
-  -1/2 * log(2 * pi * sigma^2) - 1/2 * ((x - mu)^2)/(sigma^2)
+#' Define mean log likelihood function
+log_likelihood_function <- function(mu, sigma, X) {
+  (-1/2) * log(2*pi) + (-1/2) * log(sigma^2) - 1/(2*sigma^2) * (X - mu)^2
 }
 
-mu_ls <- seq(from = 0, to = 4, by = 0.01)
-sigma_ls <- seq(from = 1, to = 5, by = 0.01)
+#' Generate grid
+library(tidyverse)
 
-df <- crossing(mu_ls, sigma_ls) %>%
-  rowwise() %>%
-  mutate(mll = log_likelihood_function(mu_ls, sigma_ls, X) %>%
-           mean())
+mu_vec <- seq(from = 0, to = 2, by = 0.01)
+sigma_vec <- seq(from = 1, to = 3, by = 0.01)
 
-ggplot(df, aes(mu_ls, sigma_ls)) +
-  geom_contour(aes(z = mll), )
+grid <- crossing(mu = mu_vec, sigma = sigma_vec) %>%
+  mutate(mean_likelihood = map2_dbl(mu, sigma, function(mu, sigma, X) mean(log_likelihood_function(mu, sigma, X)), X))
+
+#' Maximum mean log likelihood value and maximum likelihood estimates.
+MLE <- grid[grid$mean_likelihood == max(grid$mean_likelihood), ]
+knitr::kable(MLE)
+
+#' Draw plot
+plot <- ggplot(grid) +
+  geom_point(aes(x = MLE$mu, y = MLE$sigma)) +
+  annotate(geom = "text", x = MLE$mu + 0.05, y = MLE$sigma + 0.05, label = "MLE") +
+  geom_contour(aes(x = mu, y = sigma, z = mean_likelihood), binwidth = 0.025) +
+  
+  labs(x = "Mean", y = "Standard Deviation") +
+  ggthemes::theme_few(base_size = 12, base_family = "sans")
+
+ggsave(filename = "Q2.pdf", plot = plot, device = "pdf", path = "HW/output/figure/HW1/")
+
--- HW/code/HW1_Q5.R
+++ HW/code/HW1_Q5.R
@@ -5,8 +5,35 @@ theta <- 0.1
 mu <- 1.5
 sigma_2 <- 1.9
 
-E <- rnorm(n = 1000, mean = mu, sd = sqrt(sigma_2))
-lagE <- append(0, lag(E, 1)[2:1000])
+n <- 1000
 
-y <- mu + E + theta * lagE
+e_vec <- rnorm(n = n, mean = mu, sd = sqrt(sigma_2))
+lag_e_vec <- append(0, lag(e_vec, 1)[2:1000])
 
+Y <- mu + e_vec + theta * lag_e_vec
+
+log_likelihood_function <- function(mu, sigma_2, theta, Y) {
+  e <- 0
+  
+  for (i in 1:n) {
+    eforward = Y[i] - mu - theta * e[i]
+    
+    e <- append(e, eforward)
+  }
+  
+  (-n/2) * log(2*pi) + (-n/2) * log(sigma_2) - 1/(2*sigma_2) * sum((e[1:n + 1])^2)
+  
+}
+
+mu_vec <- runif(10000, min = 0, max = 3)
+sigma_2_vec <- runif(10000, min = 1, max = 3)
+theta_vec <- runif(10000, min = 0, max = 0.99)
+
+grid <- tibble(mu = mu_vec,
+               sigma_2 = sigma_2_vec,
+               theta = theta_vec) %>%
+  mutate(likelihood = pmap_dbl(list(mu, sigma_2, theta), log_likelihood_function, Y))
+
+#' Maximum mean log likelihood value and maximum likelihood estimates.
+MLE <- grid[grid$likelihood == max(grid$likelihood), ]
+knitr::kable(MLE)
