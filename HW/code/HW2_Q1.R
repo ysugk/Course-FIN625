a5edb7560891b7df8a24e3aca01514bac88502e6
Yongseok <ysugk@snu.ac.kr>
2020-03-23 20:13:02 -0400

    add caption

--- HW/code/HW2_Q1.R
+++ HW/code/HW2_Q1.R
@@ -10,28 +10,34 @@ df <- readxl::read_excel("HW/input/Problem5.2_data.xlsx") %>%
          lag1_xp = lag(xp, 1))
 
 #' a.
+# Run regressions
 m.a.1 <- lm(Rm - Rf ~ lag1_dp, df)
 m.a.2 <- lm(Rm - Rf ~ lag1_xp, df)
 
+#' Fitted value and realized value
 y <- (df$Rm - df$Rf)[2:581]
 yhat1 <- m.a.1$fitted.values
 yhat2 <- m.a.2$fitted.values
 
+#' Calculate R-squared
 rsq.a.1 <- sum(yhat1^2)/sum(y^2)
 rsq.a.2 <- sum(yhat2^2)/sum(y^2)
 
+#' Report tabular
 data.frame(predictor = c("dp", "xp"),
            rsq = c(rsq.a.1, rsq.a.2)) %>%
   stargazer(type = "text", summary = FALSE, rownames = FALSE, float = FALSE, out = "HW/output/tabs/HW2/Q1.a.tex")
 
 #' b.
 
+#' Define subsample function
 subsample <- function(x, k = (1926 - 1872 + 1) * 4){
   indexes <- 1:(length(x) - k + 1)
   
   map(indexes, function(i) x[i:(i + k - 1)])
 }
 
+#' Obtain lists of subsample
 rm_ls <- subsample(df$Rm[2:581])
 rf_ls <- subsample(df$Rf[2:581])
 
@@ -39,6 +45,7 @@ y_ls <- subsample(y)
 dp_ls <- subsample(df$lag1_dp[2:581])
 xp_ls <- subsample(df$lag1_xp[2:581])
 
+#' Define function to calculate rolling coefficients
 calculate_ols_coef <- function(y, x){
   X <- matrix(c(rep(1, length(x)), x), nrow = length(x), ncol = 2)
   
@@ -47,15 +54,18 @@ calculate_ols_coef <- function(y, x){
   coef
 } 
 
+#' Obtain coefficients
 coef_ls1 <- map2(y_ls, dp_ls, calculate_ols_coef)
 coef_ls2 <- map2(y_ls, xp_ls, calculate_ols_coef)
 
+#' Construct out-of-sample
 df_os <- filter(df, Date >= ymd(19270101))
 rm_os <- df_os$Rm
 rf_os <- df_os$Rf
 dp_os <- df_os$lag1_dp
 xp_os <- df_os$lag1_xp
 
+#' Define a function to construct predicted value
 calculate_predicted_value <- function(x, coef_ls){
   coef <- coef_ls %>%
     bind_cols()
@@ -70,13 +80,16 @@ calculate_predicted_value <- function(x, coef_ls){
   alpha[1:n] + beta[1:n] * x
 }
 
+#' Obtain predicted value
 rmhat1 <- calculate_predicted_value(dp_os, coef_ls1) + rf_os
 rmhat2 <- calculate_predicted_value(xp_os, coef_ls2) + rf_os
 rmbar <- map_dbl(rm_ls, mean)[1:360]
 
+#' Calculate out-of-sample statistics from Welch and Goyal, 2008
 rsq_os.b.1 <- 1 - sum((rm_os - rmhat1)^2)/sum((rm_os - rmbar)^2)
 rsq_os.b.2 <- 1 - sum((rm_os - rmhat2)^2)/sum((rm_os - rmbar)^2)
 
+#' Calculate in-sample R-squared for the out-of-sample period
 m.b.1 <- lm(rm_os - rf_os ~ dp_os)
 m.b.2 <- lm(rm_os - rf_os ~ xp_os)
 
@@ -87,6 +100,7 @@ y <- rm_os - rf_os
 rsq.b.1 <- sum(yhat1^2)/sum(y^2)
 rsq.b.2 <- sum(yhat2^2)/sum(y^2)
 
+#' Report tabular
 data.frame(predictor = c("dp", "xp"),
            rsq_os = c(rsq_os.b.1, rsq_os.b.2),
            rsq_is = c(rsq.b.1, rsq.b.2)) %>%
@@ -94,6 +108,7 @@ data.frame(predictor = c("dp", "xp"),
 
 #' c.
 
+#' Define a function to modify predicted value following Campbell and Thompson, 2008
 calculate_modified_predicted_value <- function(x, coef_ls){
   coef <- coef_ls %>%
     bind_cols()
@@ -110,21 +125,25 @@ calculate_modified_predicted_value <- function(x, coef_ls){
   map_dbl(alpha[1:n] + mbeta[1:n] * x, function(z) max(z, 0))
 }
 
-xbar1 <- cummean(df$lag1_dp[2:581])[(580 - 360 + 1):580]
-xbar2 <- cummean(df$lag1_xp[2:581])[(580 - 360 + 1):580]
+#' Generate predicted value
+xbar1 <- df_os$lag1_dp
+xbar2 <- df_os$lag1_xp
 
 rmhat1 <- calculate_modified_predicted_value(xbar1, coef_ls1) + rf_os
 rmhat2 <- calculate_modified_predicted_value(xbar2, coef_ls2) + rf_os
 
+#' Calculate out-of-sample R-squared
 rsq_os.c.1 <- 1 - sum((rm_os - rmhat1)^2)/sum((rm_os - rmbar)^2)
 rsq_os.c.2 <- 1 - sum((rm_os - rmhat2)^2)/sum((rm_os - rmbar)^2)
 
+#' Report tabular
 data.frame(predictor = c("dp", "xp"),
            rsq_os = c(rsq_os.c.1, rsq_os.c.2)) %>%
   stargazer(type = "text", summary = FALSE, rownames = FALSE, float = FALSE, out = "HW/output/tabs/HW2/Q1.c.tex")
 
 
 #' d.
+#' Generate our new predictor
 D <- df$D
 
 G <- ((D - lag(D, 1))/lag(D, 1))[2:581]
@@ -144,7 +163,7 @@ x <- dp * (1 + G) + exp(Eg) + 0.5 * varr
 summary(x)
 
 #' e.
-
+#' repeat the calculation of part b
 y <- (df$Rm - df$Rf)[3:581]
 y_ls <- subsample(y)
 x_ls <- subsample(x[1:579])
@@ -168,21 +187,25 @@ y <- rm_os - rf_os
 
 rsq.e.1 <- sum(yhat^2)/sum(y^2)
 
-xbar <- cummean(x)[(579 - 360 + 2):579]
+#' Repeat the calculation of part c
+xbar <- x[(579 - 360 + 2):579]
 rmhat <- calculate_modified_predicted_value(xbar, coef_ls) + rf_os
 rsq_os.e.2 <- 1 - sum((rm_os - rmhat)^2)/sum((rm_os - rmbar)^2)
 
+#' Report tabular
 data.frame(part = c("b)", "c)"),
            rsq_os = c(rsq_os.e.1, rsq_os.e.2),
            rsq_is = c(rsq.e.1, NA)) %>%
   stargazer(type = "text", summary = FALSE, rownames = FALSE, float = FALSE, out = "HW/output/tabs/HW2/Q1.e.tex")
 
 #' f.
+#' Calculate out-of-sample R-squared when the theoretical restrictions are fully imposed
 rmhat <- x - 1
 rmhat <- rmhat[(579 - 360 + 2):579]
 
 rsq_os.f <- 1 - sum((rm_os - rmhat)^2)/sum((rm_os - rmbar)^2)
 
+#' Report tabular
 data.frame(restriction = "a = -1, b = 1",
            rsq_os = rsq_os.f) %>%
   stargazer(type = "text", summary = FALSE, rownames = FALSE, float = FALSE, out = "HW/output/tabs/HW2/Q1.f.tex")
